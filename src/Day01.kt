009155c12b06c4b94010f022b82634635a7aa175
Timothy Jacobson <tim.jacobson@motixconnected.com>
2022-12-01 09:48:30 +0000

    Day 1 - Part 1
    Signed-off-by: Timothy Jacobson <tim.jacobson@motixconnected.com>

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        fun List<String>.groupByBlankLine() = buildMap<Int, List<String>> {
+            var i = 0
+            this@groupByBlankLine.forEach {
+                if (it.isBlank()) i++
+                else this.compute(i) { _, e ->
+                    if (e == null) listOf(it)
+                    else (e + it)
+                }
+            }
+        }
+        return input.groupByBlankLine()
+            .mapValues { entry -> entry.value.map { it.toInt() } }
+            .maxOf { entry -> entry.value.sum() }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
